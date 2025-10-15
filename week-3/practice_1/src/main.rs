c11a9ec1a56dd1f05be1d1fb72c10ba341686cbc
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2025-10-15 16:01:54 +0100

    practice 1

---
+++ week-3/practice_1/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let school_string = "school of science and technology";  // string type
+    let rating_float = 5.0; // float type
+    let is_growing_boolean = true; //boolean type
+
+
+    println!("school Name is :{}",school_string);
+    println!("school rating :{}", rating_float);
+    println!("school is growing:{}",)is_growing_boolean;
+    println!("school of departments : {}",department integer);
+}
+
+    
