35fec7a973dbffaceb8703a757f77ae7a56ddcff
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2025-10-15 18:38:30 +0100

    practice_4

---
+++ practice_4/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = "B"
+    let surname = "Bolaji";
+    let first_name = "Michael";
+    let middle first_name = "Gboyega";
+
+    println!(" ")
+    println!("special character: {}",special_character);
+    println!("Alphabet: {}",surname, middle_name, first_name);
+}
