0c457fc29aae2efe5d6cc8ce158fb2dd5638ee8d
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2025-10-22 19:08:07 +0100

    practice_4

---
+++ week_4/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\s.kassimCSC101\week_4\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\NEW\s.kassimCSC101\week_4\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
