f069597f4143f9a57ae3686d9cc21b68417850ae
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2025-11-19 18:38:16 +0100

    Mutable slice in rust

---
+++ week_8/practice_12/target/debug/deps/practice_12.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_12\target\debug\deps\practice_12.d: src\main.rs
+
+C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_12\target\debug\deps\practice_12.exe: src\main.rs
+
+src\main.rs:
---
+++ week_8/practice_12/target/debug/practice_12.d
@@ -0,0 +1,1 @@
+C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_12\target\debug\practice_12.exe: C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_12\src\main.rs
