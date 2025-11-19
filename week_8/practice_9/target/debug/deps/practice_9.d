06d984763f7235eb1bd1f886c55d643772f59f08
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2025-11-19 18:28:37 +0100

    Passing a tuble as a parameter

---
+++ week_8/practice_9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
---
+++ week_8/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_9\target\debug\practice_9.exe: C:\Users\NEW\documents\s.kassimCSC101\week_8\practice_9\src\main.rs
