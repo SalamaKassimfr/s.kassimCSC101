bab00c7d7ff5f1af18b9e40801af8b29f7485c2c
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2025-11-26 21:18:03 +0100

    Read from a file

---
+++ week_9/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\documents\s.kassimCSC101\week_9\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\NEW\documents\s.kassimCSC101\week_9\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
