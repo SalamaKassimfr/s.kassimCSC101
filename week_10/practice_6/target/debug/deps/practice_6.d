311065c936b1b20e8720e5d6631c84d3b6d3f24d
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2025-12-03 16:54:34 +0100

    Define struct for basic data

---
+++ week_10/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\Documents\s.kassimCSC101\week_10\practice_6\target\debug\deps\practice_6.d: src\main.rs
+
+C:\Users\NEW\Documents\s.kassimCSC101\week_10\practice_6\target\debug\deps\practice_6.exe: src\main.rs
+
+src\main.rs:
---
+++ week_10/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\NEW\Documents\s.kassimCSC101\week_10\practice_6\target\debug\practice_6.exe: C:\Users\NEW\Documents\s.kassimCSC101\week_10\practice_6\src\main.rs
